82b6214ec394474fb582d745007ec912e4bb7015
kimmybrindle <kimmybrindle@gmail.com>
2025-10-16 23:34:00 +1100

    added ids and added main js functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+function cityDropdown(event) {
+  event.preventDefault();
+  if (event.target.value.length > 0) {
+    if (event.target.value === "london") {
+      alert(`It is ${londonDate} in Europe/London.`);
+    }
+    if (event.target.value === "los angeles") {
+      alert(`It is ${LosAngelesDate} in America/Los Angeles.`);
+    }
+    if (event.target.value === "auckland") {
+      alert(`It is ${aucklandDate} in Pacific/Auckland.`);
+    }
+  }
+}
+let londonDate = moment()
+  .tz("Europe/London")
+  .format("dddd MMMM Do, YYYY h:mm A");
 
+let londonOption = document.querySelector("#london");
+
+let LosAngelesDate = moment()
+  .tz("America/Los_Angeles")
+  .format("dddd MMMM Do, YYYY h:mm A");
+
+let losAngelesOption = document.querySelector("#los-angeles");
+
+let aucklandDate = moment()
+  .tz("Pacific/Auckland")
+  .format("dddd MMMM Do, YYYY h:mm A");
+let aucklandOption = document.querySelector("#auckland");
+
+let formElement = document.querySelector("form");
+formElement.addEventListener("change", cityDropdown);
