dcd043aa70cec7c31d8dfbab296cc42d0399c035
kimmybrindle <kimmybrindle@gmail.com>
2025-10-17 10:07:36 +1100

    updated html values and simplified js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
-function cityDropdown(event) {
-  event.preventDefault();
+function showDate(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "london") {
-      alert(`It is ${londonDate} in Europe/London.`);
-    }
-    if (event.target.value === "los angeles") {
-      alert(`It is ${LosAngelesDate} in America/Los Angeles.`);
-    }
-    if (event.target.value === "auckland") {
-      alert(`It is ${aucklandDate} in Pacific/Auckland.`);
-    }
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
-let londonDate = moment()
-  .tz("Europe/London")
-  .format("dddd MMMM Do, YYYY h:mm A");
 
-let londonOption = document.querySelector("#london");
-
-let LosAngelesDate = moment()
-  .tz("America/Los_Angeles")
-  .format("dddd MMMM Do, YYYY h:mm A");
-
-let losAngelesOption = document.querySelector("#los-angeles");
-
-let aucklandDate = moment()
-  .tz("Pacific/Auckland")
-  .format("dddd MMMM Do, YYYY h:mm A");
-let aucklandOption = document.querySelector("#auckland");
-
-let formElement = document.querySelector("form");
-formElement.addEventListener("change", cityDropdown);
+let selectElement = document.querySelector("form");
+selectElement.addEventListener("change", showDate);
